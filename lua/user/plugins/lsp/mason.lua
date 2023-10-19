6ec754c1fdf343cf2bc6a74a80f97bb25fafa8b2
Jesse Frohlich <jesse@frohli.ch>
2023-10-19 14:05:30 -0400

    Add plugins relevant to LSP configuration.

---
+++ lua/user/plugins/lsp/mason.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim"
+        },
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                },
+            },
+        },
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+        },
+    },
+}
