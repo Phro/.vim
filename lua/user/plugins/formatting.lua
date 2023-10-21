832f66135e04aae770580d3b2817055295ea85dd
Jesse Frohlich <jesse@frohli.ch>
2023-10-20 23:01:52 -0400

    Add formatter `conform.nvim`.

---
+++ lua/user/plugins/formatting.lua
@@ -0,0 +1,37 @@
+return {
+	"stevearc/conform.nvim",
+	lazy = true,
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<Leader>W",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { "prettierd" },
+			typescript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			css = { "prettierd" },
+			html = { "prettierd" },
+			json = { "prettierd" },
+			yaml = { "prettierd" },
+			markdown = { "prettierd" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			timeout_ms = 500,
+		},
+	},
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+}
