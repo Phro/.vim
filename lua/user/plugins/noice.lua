027c18a2629d49013848d562161975145d54ae7f
Jesse Frohlich <jesse@frohli.ch>
2023-10-22 21:46:53 -0400

    Add commands for `noice`: dismiss messages, view previous messages, &c.

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -5,6 +5,26 @@ return {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
 	},
+	keys = {
+		{
+			"<Leader>nh",
+			function()
+				require("noice").cmd("history")
+			end,
+		},
+		{
+			"<Leader>nd",
+			function()
+				require("noice").cmd("dismiss")
+			end,
+		},
+		{
+			"<Leader>nl",
+			function()
+				require("noice").cmd("last")
+			end,
+		},
+	},
 	opts = {
 		presets = {
 			command_palette = true,
