cfdd0a05e096655057ff02cc91e45fdba56d001f
Jesse Frohlich <jesse@frohli.ch>
2023-10-28 22:40:37 -0400

    Regularize formatting of `/lsp` directory.

--- lua/user/plugins/lsp/mason.lua
+++ lua/user/plugins/lsp/mason.lua
@@ -1,30 +1,30 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                },
-            },
-        },
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = {
-            "williamboman/mason.nvim"
-        },
-        opts = {
-            ensure_installed = {
-                "tsserver",
-                "html",
-                "cssls",
-                "tailwindcss",
-                "lua_ls",
-            },
-            automatic_installation = true,
-        },
-    },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		opts = {
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+			},
+			automatic_installation = true,
+		},
+	},
 }
